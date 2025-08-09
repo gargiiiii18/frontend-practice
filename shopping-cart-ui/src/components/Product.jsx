e0539b7df742c2d86c059fcd46b9007bd485be3d
gargi <gargi.p.sukhatankar@gmail.com>
2025-08-10 02:45:15 +0530

    updated page

--- shopping-cart-ui/src/components/Product.jsx
+++ shopping-cart-ui/src/components/Product.jsx
@@ -31,7 +31,7 @@ const product = () => {
                     <h2 className='font-semibold text-blue-900 text-lg'>₹{item.price}</h2>
                     <div className='flex gap-2 justify-around items-center py-2 w-28 px-4 rounded-2xl bg-blue-200'>
                         <button className='text-center bg-transparent text-blue-900 font-semibold'>+</button>
-                        <p className='bg-white w-10 p-1 rounded-sm text-black text-center text-sm'>qty</p>
+                        <p className='bg-white w-10 p-1 rounded-sm text-black text-center text-sm'>1</p>
                         <button className='text-center text-blue-900 font-semibold'>-</button>
                     </div>
                     <button onClick={() => { removeItem(item.id) }} className=' px-3 py-2 bg-red-500 rounded-xl text-sm font-semibold text-white'>Remove</button>
