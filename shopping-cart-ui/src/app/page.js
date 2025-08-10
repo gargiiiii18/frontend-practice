7600da842830313b8a83aff02f95a56c56afe2f9
gargi <gargi.p.sukhatankar@gmail.com>
2025-08-10 14:09:44 +0530

    updated files

--- shopping-cart-ui/src/app/page.js
+++ shopping-cart-ui/src/app/page.js
@@ -4,7 +4,7 @@ import Product from "@/components/Product";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <section className="p-8 m-4 shadow-sm inset-0 mx-auto rounded-2xl  w-fit bg-blue-200">
+      <section className=" p-8 m-4 shadow-sm inset-0 mx-auto rounded-2xl  w-fit bg-blue-200">
         <h1 className="text-center mb-4 p-2 text-blue-800 font-sans font-bold text-3xl">Shopping Cart 🛒</h1>
       <Product/>
       </section>
--- shopping-cart-ui/src/components/Product.jsx
+++ shopping-cart-ui/src/components/Product.jsx
@@ -42,21 +42,24 @@ const product = () => {
      <div className='m-2 p-4 flex flex-col gap-4'>
     {cartItems.map(item => (
        
-        <div key={item.id} className='bg-blue-100 rounded-2xl m-1 p-4 flex gap-10 shadow-sm'>
-            <div className='bg-white rounded-2xl shadow-sm'>
-                <img className='h-[150px] rounded-2xl' src={item.imageUrl} alt="" />
+        <div key={item.id} className='bg-blue-100 rounded-2xl m-1 p-4 w-80 md:w-full flex flex-col md:flex-row gap-10 shadow-sm'>
+            <div className='bg-white rounded-2xl flex justify-center items-center shadow-sm'>
+                <img className='h-[150px] sel rounded-2xl' src={item.imageUrl} alt="" />
             </div>
             <div className='flex flex-col gap-4'>
+                {/* <img className='h-[150px] rounded-2xl' src={item.imageUrl} alt="" /> */}
                 <h1 className='font-semibold text-black text-md'>{item.name}</h1>
                 <p className='text-gray-600 text-sm mx-1 my-3'>{item.description}</p>
-                <div className='flex w-90 justify-between items-center'>
+                <div className='flex flex-col gap-6 md:flex-row md:w-90 justify-center md:justify-between md:items-center'>
                     <h2 className='font-semibold text-blue-900 text-lg'>₹{item.price}</h2>
+                    <div className='flex gap-15 justify-center items-center'>
                     <div className='flex gap-2 justify-around items-center py-2 w-28 px-4 rounded-2xl bg-blue-200'>
                         <button onClick={() => {increment(item.id)}} className='text-center bg-transparent text-blue-900 font-semibold'>+</button>
                         <p className='bg-white w-10 p-1 rounded-sm text-black text-center text-sm'>{item.quantity}</p>
                         <button onClick={() => {decrement(item.id)}} className='text-center text-blue-900 font-semibold'>-</button>
                     </div>
                     <button onClick={() => { removeItem(item.id) }} className=' px-3 py-2 bg-red-500 rounded-xl text-sm font-semibold text-white'>Remove</button>
+                    </div>
                 </div>
             </div>
         </div>
